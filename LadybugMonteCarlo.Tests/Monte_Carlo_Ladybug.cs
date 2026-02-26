d4d8f3dbd5343a78705baa2c5796f9744cd0b006
lars-erik <lars-erik@aabech.no>
2026-02-27 00:11:19 +0100

    Moved OOP version into its own file

--- LadybugMonteCarlo.Tests/Monte_Carlo_Ladybug.cs
+++ LadybugMonteCarlo.Tests/Monte_Carlo_Ladybug.cs
@@ -1,7 +1,8 @@
-﻿using Microsoft.Testing.Platform.Logging;
+﻿using LadybugMonteCarlo.Tests.OopLadybug;
 
 namespace LadybugMonteCarlo.Tests;
 
+[Parallelizable]
 public class Monte_Carlo_Ladybug
 {
     private Ladybug bug;
@@ -72,110 +73,61 @@ public class Monte_Carlo_Ladybug
                 .Property(nameof(ClockRun.FinalHour)).EqualTo(expectedPosition));
     }
 
-    [TestCase(1, 10_000, "875, 962, 931, 942, 873, 924, 918, 873, 869, 914, 919")]
+    [TestCase(1,  10_000,            "875, 962, 931, 942, 873, 924, 918, 873, 869, 914, 919")]
+    [TestCase(2,  10_000,            "935, 937, 892, 884, 872, 909, 924, 915, 916, 933, 883")]
     [TestCase(1, 100_000, "9202, 9200, 9027, 9153, 9184, 9062, 8994, 9088, 8930, 9149, 9011")]
-    [TestCase(2, 10_000, "935, 937, 892, 884, 872, 909, 924, 915, 916, 933, 883")]
     public void Simulator_Builds_Histogram(int seed, int cases, string expectedDistributionCsv)
     {
         var simulator = new BugSimulator(seed);
 
         simulator.RunCases(cases);
 
-        var distribution = simulator.GetDistribution();
+        var dataset = simulator.GetDistribution();
 
-        foreach (var item in distribution)
+        foreach (var item in dataset.Distribution)
         {
             Console.WriteLine($"{item.Key, 2}: {item.Value, 6}");
         }
-        Console.WriteLine($"   {distribution.Values.Sum(), 7:# ##0}");
+        Console.WriteLine($"   {dataset.Cases, 7:# ##0}");
 
         Assert.Multiple(() =>
         {
-            Assert.That(distribution.Values.Sum(), Is.EqualTo(cases));
-            Assert.That(distribution.Values, Is.EquivalentTo(expectedDistributionCsv.Split(", ").Select(int.Parse)));
+            Assert.That(dataset.Cases, Is.EqualTo(cases));
+            Assert.That(dataset.Values, Is.EquivalentTo(expectedDistributionCsv.Split(", ").Select(int.Parse)));
         });
     }
 
     private const double ExpectedChance = 1.0 / 11.0;
 
-    [TestCase( 1,    10_000, 0.006  )]
-    [TestCase( 1,   100_000, 0.002  )]
-    [TestCase( 1,   500_000, 0.0007 )]
-    [TestCase( 1, 1_000_000, 0.0007 )]
-    [TestCase( 2,    10_000, 0.006  )]
-    [TestCase( 2,   100_000, 0.002  )]
-    [TestCase( 2,   500_000, 0.0012 )]
+    [TestCase( 1,    10_000, 0.006    )]
+    [TestCase( 2,    10_000, 0.006    )]
+    [TestCase( 1,   100_000, 0.002    )]
+    [TestCase( 2,   100_000, 0.002    )]
+    [TestCase( 1,   500_000, 0.0007   )]
+    [TestCase( 2,   500_000, 0.0012   )]
+    [TestCase( 1, 1_000_000, 0.0007   )]
     [TestCase( 2, 1_000_000, 0.000603 )]
     public void Simulator_Finds_Equal_Chance(int seed, int cases, double tolerance)
     {
         var simulator = new BugSimulator(seed);
         simulator.RunCases(cases);
-        var distribution = simulator.GetDistribution();
+        var dataset = simulator.GetDistribution();
 
         Console.WriteLine("    Count  Freq     Dev");
-        foreach (var item in distribution)
+        foreach (var item in dataset.Distribution)
         {
             var itemFreq = (double)item.Value / cases;
             Console.WriteLine($"{item.Key,2}: {item.Value,6} {itemFreq,6:P4} {itemFreq - ExpectedChance,6:P4}");
         }
 
-        var avg = distribution.Values.Select(x => (double)x / cases).ToArray();
-
-        Console.WriteLine($"Avg/Max:   {avg.Average(),6:P4} {avg.Select(x => x - ExpectedChance).MaxBy(Math.Abs),6:P4}");
+        var maxDifference = dataset.MaxDifference(ExpectedChance);
+        Console.WriteLine($"Avg/Max:   {dataset.AverageFrequency,6:P4} {maxDifference,6:P4}");
 
         Assert.Multiple(() =>
         {
-            Assert.That(avg, Has.All.EqualTo(ExpectedChance).Within(tolerance));
-            Assert.That(avg.Average(), Is.EqualTo(ExpectedChance).Within(tolerance));
+            Assert.That(dataset.Frequencies.Values, Has.All.EqualTo(ExpectedChance).Within(tolerance));
+            Assert.That(dataset.AverageFrequency, Is.EqualTo(ExpectedChance).Within(tolerance));
         });
     }
 }
 
-public record ClockRun(int Rounds, int FinalHour, HashSet<int> VisitedHours, List<int> Log);
-
-public record Ladybug()
-{
-    public int Position { get; private set; } = 12;
-
-    public void GoRight() => Position = Position % 12 + 1;
-
-    public void GoLeft() => Position = (Position + 10) % 12 + 1;
-}
-
-public class BugSimulator(int seed)
-{
-    private readonly Random random = new(seed);
-
-    public List<ClockRun> Runs { get; } = new();
-
-    public void RunCases(int count)
-    {
-        for (var i = 0; i < count; i++)
-        {
-            RunCase();
-        }
-    }
-
-    public void RunCase()
-    {
-        var bug = new Ladybug();
-        var set = new HashSet<int> { 12 };
-        var log = new List<int> { 12 };
-        while(set.Count < 12)
-        {
-            Action move = random.Next(2) == 0 ? bug.GoLeft : bug.GoRight;
-            move();
-            set.Add(bug.Position);
-            log.Add(bug.Position);
-        }
-        Runs.Add(new ClockRun(log.Count, bug.Position, set, log));
-    }
-
-    public Dictionary<int, int> GetDistribution()
-    {
-        return Runs
-            .GroupBy(x => x.FinalHour)
-            .OrderBy(g => g.Key)
-            .ToDictionary(x => x.Key, x => x.Count());
-    }
-}
---
+++ LadybugMonteCarlo.Tests/OopLadybug.cs
@@ -0,0 +1,84 @@
+﻿namespace LadybugMonteCarlo.Tests.OopLadybug;
+
+public record ClockRun(int Rounds, int FinalHour, HashSet<int> VisitedHours, List<int> Log);
+
+public record Ladybug()
+{
+    public int Position { get; private set; } = 12;
+
+    public void GoRight() => Position = Position % 12 + 1;
+
+    public void GoLeft() => Position = (Position + 10) % 12 + 1;
+}
+
+public class BugSimulator(int seed)
+{
+    private readonly Random random = new(seed);
+
+    public List<ClockRun> Runs { get; } = new();
+
+    public void RunCases(int count)
+    {
+        for (var i = 0; i < count; i++)
+        {
+            RunCase();
+        }
+    }
+
+    public void RunCase()
+    {
+        var bug = new Ladybug();
+        var set = new HashSet<int> { 12 };
+        var log = new List<int> { 12 };
+
+        while (set.Count < 12)
+        {
+            Action move =
+                random.Next(2) == 0
+                ? bug.GoLeft
+                : bug.GoRight;
+
+            move();
+
+            set.Add(bug.Position);
+            log.Add(bug.Position);
+        }
+
+        Runs.Add(new ClockRun(log.Count, bug.Position, set, log));
+    }
+
+    public DistributionDataset GetDistribution()
+    {
+        return new DistributionDataset(Runs);
+    }
+}
+
+public record DistributionDataset
+{
+    public Dictionary<int, int> Distribution { get; }
+    public IReadOnlyDictionary<int, double> Frequencies { get; }
+    public int Cases { get; }
+    public IReadOnlyCollection<int> Values { get; }
+    public double AverageFrequency { get; }
+
+    public DistributionDataset(List<ClockRun> runs)
+    {
+        Cases = runs.Count;
+
+        Distribution = runs
+            .GroupBy(x => x.FinalHour)
+            .OrderBy(g => g.Key)
+            .ToDictionary(x => x.Key, x => x.Count());
+
+        Frequencies = Distribution.ToDictionary(x => x.Key, x => (double)x.Value / Cases).AsReadOnly();
+        AverageFrequency = Frequencies.Values.Average();
+
+        Values = Distribution.Values.ToArray();
+
+    }
+
+    public double MaxDifference(double expectedChance)
+    {
+        return Frequencies.Values.Select(x => x - expectedChance).MaxBy(Math.Abs);
+    }
+}
