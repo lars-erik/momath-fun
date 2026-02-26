c698c6ae20678de0ddd19c4956f955aa010d885d
lars-erik <lars-erik@aabech.no>
2026-02-26 22:12:02 +0100

    Add project files.

---
+++ LadybugMonteCarlo.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿namespace LadybugMonteCarlo.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
