837a7c8d6853b9c4371b376bd167089e7a92a83b
lars-erik <lars-erik@aabech.no>
2026-02-27 14:40:31 +0100

    Multithreaded 5 mil C# sim in 700 ms.

--- LadybugMonteCarlo.Tests/Golfed_Ladybug.cs
+++ LadybugMonteCarlo.Tests/Golfed_Ladybug.cs
@@ -1,4 +1,5 @@
-﻿namespace LadybugMonteCarlo.Tests;
+﻿// ReSharper disable PossibleMultipleEnumeration
+namespace LadybugMonteCarlo.Tests;
 
 public class Golfed_Ladybug
 {
@@ -20,29 +21,54 @@ public class Golfed_Ladybug
         Assert.That(result.Frequencies.Values, Has.All.EqualTo(1.0 / 11).Within(.0055));
     }
 
+    [Test]
+    public async Task Multithreaded_Procedural_5Mil()
+    {
+        var cases = 5_000_000;
+        var threads = 16;
+
+        var batchSize = cases / threads;
+
+        var tasks = Enumerable.Range(0, threads)
+            .Select(i => Task.Run(() => MakeRuns(i, batchSize)))
+            .ToArray();
+        var batchResults = await Task.WhenAll(tasks);
+        var results = batchResults.SelectMany(x => x);
+        var result = new GolfDataset(results);
+        
+        Assert.That(results.Count(), Is.EqualTo(5_000_000));
+        Assert.That(result.Frequencies.Values, Has.All.EqualTo(1.0 / 11).Within(.0055));
+        Assert.That(result.MinMaxRounds, Is.EqualTo(new Tuple<int, int>(11, 544)));
+    }
+
     struct GolfDataset
     {
         public IReadOnlyDictionary<int, int> Distribution { get; }
         public IReadOnlyDictionary<int, double> Frequencies { get; }
+        public Tuple<int, int> MinMaxRounds { get; }
         public double AverageFrequency { get; }
 
-        public GolfDataset(ICollection<int> results)
+        public GolfDataset(IEnumerable<(int location, int rounds)> results)
         {
             Distribution = results
-                .GroupBy(x => x)
+                .GroupBy(x => x.location)
                 .OrderBy(g => g.Key)
                 .ToDictionary(x => x.Key, x => x.Count());
-            Frequencies = Distribution.ToDictionary(x => x.Key, x => (double)x.Value / results.Count).AsReadOnly();
+            Frequencies = Distribution.ToDictionary(x => x.Key, x => (double)x.Value / results.Count()).AsReadOnly();
             AverageFrequency = Frequencies.Values.Average();
+            MinMaxRounds = new Tuple<int, int>(
+                results.Min(x => x.rounds),
+                results.Max(x => x.rounds)
+            );
         }
     }
 
-    private static GolfDataset FromResults(ICollection<int> results)
+    private static GolfDataset FromResults(IEnumerable<(int location, int rounds)> results)
     {
         return new GolfDataset(results);
     }
 
-    private static int[] MakeRuns(int seed, int cases)
+    private static (int location, int rounds)[] MakeRuns(int seed, int cases)
     {
         var random = new Random(seed);
         return Enumerable.Range(0, cases)
@@ -50,20 +76,22 @@ public class Golfed_Ladybug
             .ToArray();
     }
 
-    private static int MakeClockRun(Random random)
+    private static (int location, int rounds) MakeClockRun(Random random)
     {
         var location = 0;
         var visitedCount = 1;
         var visited = new[]{ true, false, false, false, false, false, false, false, false, false, false, false };
+        var rounds = 0;
         while (visitedCount < 12)
         {
             int move = random.Next(2) == 0 ? -1 : 1;
             location = (location + move + 12) % 12;
+            rounds++;
             if (visited[location]) continue;
             visited[location] = true;
             visitedCount++;
         }
 
-        return location;
+        return (location, rounds);
     }
 }
